df196fd9965d9bec55ec513621e2e398302cd9c9
eyecandycode <eyecandycode@gmail.com>
2019-10-14 12:56:15 +0100

    Tidied up for push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Hangman from "./components/Hangman";
 
-function App() {
+function App(props) {
+  const wordClues = [
+    { PINEAPPLE: "Kind of fruit" },
+    { ETHIOPIA: "Country in Africa" },
+    { "UNDER THE VOLCANO": "Novel by Malcolm Lowry" },
+    { HANNIBAL: "Carthaginian Commander who dazzled history with elephants" },
+    { RUMI: "Sufi Poet" },
+    { "PENNY FARTHING": "Type of old bike" },
+    { "CASSIUS CLAY": "Former name of boxing legend" },
+    { PAGANINI: "The devil's fiddler" },
+    { VENUS: "Female astral body" }
+  ];
+
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  const [guesses, setGuesses] = useState(0);
+  const [selected, setSelected] = useState([]);
+  const [random, setRandom] = useState(0);
+  const [game, setGame] = useState("start");
+  const [winner, setWinner] = useState(false);
+
+  if (game !== "playing") {
+    setRandom(Math.floor(Math.random() * (wordClues.length - 1)) + 1);
+    setGame("playing");
+  }
+
+  let word = Object.keys(wordClues[random])[0];
+  let clue = Object.values(wordClues[random])[0];
+
+  let wordArr = word.split("");
+
+  let toggleBlanks = wordArr.map(letter =>
+    selected.indexOf(letter) !== -1 ? (
+      letter
+    ) : letter === " " ? (
+      <span>&nbsp;&nbsp;&nbsp;</span>
+    ) : (
+      "_ "
+    )
+  );
+
+  const letterPress = e => {
+    setSelected([...selected, e.target.id]);
+    console.log(selected);
+    if (wordArr.indexOf(e.target.id) === -1) {
+      setGuesses(guesses + 1);
+    }
+    if (toggleBlanks.indexOf("_ ") === -1 && guesses < 7) {
+      setWinner(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='top'>
+        <h1>Hangman</h1>
+      </div>
+      <div className='clue'>
+        <h2>-- {clue} --</h2>
+      </div>
+      <div className='hangman'>
+        <Hangman guesses={guesses} winner={winner} />
+      </div>
+      <div className='blanks'>
+        <h2>{guesses < 7 ? toggleBlanks : "You Lose!"}</h2>
+      </div>
+      <div className='bottom'>
+        <div className='input'>
+          <h1>
+            {alphabet
+              .filter(letter => selected.indexOf(letter) === -1)
+              .map((item, i) => (
+                <button
+                  key={i}
+                  className='letter-btn'
+                  id={item}
+                  onClick={letterPress}
+                  date={Date()}
+                >
+                  {item}
+                </button>
+              ))}
+          </h1>
+        </div>
+        <div className='message'>
+          <h3>
+            {toggleBlanks.indexOf("_ ") === -1 && guesses < 7 ? "Winner!" : ""}
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Hangman.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Hangman(props) {
+    // if (props.winner) {
+    //     return <img className='crown' src="https://thumbs.gfycat.com/FreshSlipperyBrocketdeer-size_restricted.gif" alt=""/>
+    // } else {
+    if (props.guesses === 0) {
+        return <div><h3>Get guessing!</h3></div>
+    }
+    if (props.guesses === 1) {
+        return <img className='head' src="https://wojwaw.com/wp-content/themes/wojwaw/img/Geba_fazy_small.gif" alt=""/>
+    }
+    if (props.guesses === 2) {
+        return <div><img className='head' src="https://wojwaw.com/wp-content/themes/wojwaw/img/Geba_fazy_small.gif" alt=""/>
+        <br/> <img className='body' src="https://media.giphy.com/media/x2e1C2vIX825i/giphy.gif" alt=""/></div>
+    }
+    if (props.guesses === 3) {
+        return <div><img className='head' src="https://wojwaw.com/wp-content/themes/wojwaw/img/Geba_fazy_small.gif" alt=""/>
+        <br/> <span className='torso'><img className='body' src="https://media.giphy.com/media/x2e1C2vIX825i/giphy.gif" alt=""/>
+        <br/> <img className='right-arm' src="https://myrealdomain.com/images/hand-gif.gif" alt=""/> </span></div>
+
+    }
+    if (props.guesses === 4) {
+        return <div><img className='head' src="https://wojwaw.com/wp-content/themes/wojwaw/img/Geba_fazy_small.gif" alt=""/>
+        <br/> <span className='torso'><img className='body' className="left-arm" src="http://kinlawww.com/wp-content/themes/gridz/i/RIGHTARM_short.gif" alt=""/>
+        <br/> <img className='body' src="https://media.giphy.com/media/x2e1C2vIX825i/giphy.gif" alt=""/>
+        <br/> <img className='right-arm' src="https://myrealdomain.com/images/hand-gif.gif" alt=""/> </span></div>
+    }
+    
+    if (props.guesses === 5) {
+        return <div><img className='head' src="https://wojwaw.com/wp-content/themes/wojwaw/img/Geba_fazy_small.gif" alt=""/>
+        <br/> <span className='torso'><img className='body' className="left-arm" src="http://kinlawww.com/wp-content/themes/gridz/i/RIGHTARM_short.gif" alt=""/>
+        <br/> <img className='body' src="https://media.giphy.com/media/x2e1C2vIX825i/giphy.gif" alt=""/>
+        <br/> <img className='right-arm' src="https://myrealdomain.com/images/hand-gif.gif" alt=""/> </span>
+        <br/> <span className='legs'> <img className='left-leg' src="https://media3.giphy.com/media/PhI6t5v17cmP0wf8g1/giphy.gif" alt=""/> 
+        </span></div>
+    }
+    if (props.guesses === 6) {
+        return <div><img className='head' src="https://wojwaw.com/wp-content/themes/wojwaw/img/Geba_fazy_small.gif" alt=""/>
+        <br/> <span className='torso'><img className='body' className="left-arm" src="http://kinlawww.com/wp-content/themes/gridz/i/RIGHTARM_short.gif" alt=""/>
+        <br/> <img className='body' src="https://media.giphy.com/media/x2e1C2vIX825i/giphy.gif" alt=""/>
+        <br/> <img className='right-arm' src="https://myrealdomain.com/images/hand-gif.gif" alt=""/> </span>
+        <br/> <span className='legs'> <img className='left-leg' src="https://media3.giphy.com/media/PhI6t5v17cmP0wf8g1/giphy.gif" alt=""/> 
+        <br/> <img className='right-leg' src="https://media3.giphy.com/media/PhI6t5v17cmP0wf8g1/giphy.gif" alt=""/> 
+        </span></div>
+    }
+    if (props.guesses >= 7) {
+        return (<div><img className='skull' src="https://thumbs.gfycat.com/CaninePerfectCrocodile-small.gif" alt=""/></div>)
+}
+}
+//}
+export default Hangman
